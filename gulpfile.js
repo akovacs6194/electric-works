2d9a21b289bfac69f43962b14260ee9c00d7fc30
Alex <akovacs6194@gmail.com>
2018-11-09 14:49:53 -0500

    Continued development

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,9 @@
 //Import gulp tasks (watch and styles)
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import')
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-	 
-});
 
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function(){
-	
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles')
-	});
-})
+
+
+
