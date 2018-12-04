be2b77d2ab58c516e04c1223579d177cd2af2119
Alex <akovacs6194@gmail.com>
2018-12-04 09:57:42 -0500

    Completed home, about, and services page (minus pending content)

--- app/assets/scripts/solutions.js
+++ app/assets/scripts/solutions.js
@@ -1,7 +1,7 @@
 function openTab(e, tabId) {
 	var i, tabContent, tabLink;
 
-	// hide all tabContent when user clicks a button
+	// hide all tabcContent when user clicks a button
 	tabContent = document.getElementsByClassName("tabContent");
 	for (i = 0; i < tabContent.length; i++) {
 		tabContent[i].style.display = "none";
@@ -10,11 +10,11 @@ function openTab(e, tabId) {
 	//Remove active class for the tablink if it is currently active
 	tabLink = document.getElementsByClassName("tabLink");
 	for (i = 0; i < tabLink.length; i++) {
-		tabLink[i].className = tabLink[i].className.replace(" active-title", "");
+		tabLink[i].className = tabLink[i].className.replace(" sol-active-title", "");
 	}
 	//Display correct tabContent
 	document.getElementById(tabId).style.display = "block";
-	e.currentTarget.className += " active-title";
+	e.currentTarget.className += " sol-active-title";
 
 }
 
